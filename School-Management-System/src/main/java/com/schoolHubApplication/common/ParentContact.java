51848361bed9fd239402da5a5aebfe64f15f68e1
jagadeesh laveti <jagadeesh laveti@jagapatibabu>
2025-09-03 01:16:33 +0530

    commit and push

--- School-Management-System/src/main/java/com/schoolHubApplication/common/ParentContact.java
+++ School-Management-System/src/main/java/com/schoolHubApplication/common/ParentContact.java
@@ -1,5 +1,15 @@
 package com.schoolHubApplication.common;
 
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Embeddable  //without creating an object
+@Data
 public class ParentContact {
+	
+	private String name;
+	private String relation;
+	private String phoneNumber;
+	private String email;
 
 }
--- School-Management-System/src/main/java/com/schoolHubApplication/common/Student.java
+++ School-Management-System/src/main/java/com/schoolHubApplication/common/Student.java
@@ -1,37 +1,117 @@
 package com.schoolHubApplication.common;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 //STUDENT JAVA CLASS
 @Entity
 @Data
-@Table(name="Student")
+@Table(name = "STUDENT")
 public class Student {
-	
-	@Id
-	@Column(name="sid")
-	private Integer studentId;
-	@Column(name="sid")
-	private String firstName;
-	@Column(name="sid")
-	private String lastName;
-	private String email;
-	private String gradeLevel;
-	private Date dateOfBirth;
-	private String contactNumber;
-	private String address;
-	private String enrollmentDate;
-	private List<String> Courses;
-	private Double gpa;
-	private List<AttendanceRecord> attendanceRecords;
-	private ParentContact parentContact;
-	private String profilePhoto;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "STUDENT_ID")
+    private Integer studentId;
 
-	
+    @Column(name = "STUDENT_NAME")
+    private String studentFullName;
+
+    @Column(name = "FATHER_NAME")
+    private String fatherName;
+
+    @Column(name = "MOTHER_NAME")
+    private String motherName;
+
+    @Column(name = "GENDER")
+    private String gender;
+
+    @Column(name = "AADHAR_NUMBER")
+    private Long aadharNumber;
+
+    @Column(name = "DOB")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "BOARD")
+    private String board;
+
+    @Column(name = "SCHOOL")
+    private String school; 
+    
+    @Column(name = "RESERVATION")
+    private String reservationType;
+    
+    @Column(name = "CONTACT_NUMBER")
+    private String contactNumber;
+
+    @Column(name = "ADDRESS")
+    private String address;
+
+    @Column(name = "JOIN_DATE")
+    private LocalDate enrollmentDate;
+
+    @ElementCollection
+    @CollectionTable(
+        name = "STUDENT_SECTIONS",
+        joinColumns = @JoinColumn(name = "STUDENT_ID")
+    )
+    @Column(name = "SECTION")
+    private List<String> sections;
+
+    @ElementCollection
+    @CollectionTable(
+        name = "STUDENT_LANGUAGES", // name of the separate table
+        joinColumns = @JoinColumn(name = "STUDENT_ID") // foreign key to the Student table
+    )
+    @Column(name = "LANGUAGES")
+    private List<String> languages;
+
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "STUDENT_ID")
+    private List<AttendanceRecord> attendanceRecords;
+    
+    @ElementCollection
+    @CollectionTable(
+        name = "STUDENT_PARENT_CONTACTS",
+        joinColumns = @JoinColumn(name = "STUDENT_ID")
+    )
+    private List<ParentContact> parentDetails;
+
+    @Column(name = "PROFILE")
+    private String profilePhoto;
+
+//    Location Information
+    @Column(name = "STATE")
+    private String state;
+
+    @Column(name = "DISTRICT")
+    private String district;
+    
+    @Column(name = "BRANCH_TYPE")	//dayScholar
+    private String branchType;
+    
+    @Column(name = "CAMPUS")  //co-education 
+    private String campusType;
+
+    @Column(name = "BRANCH")//branch of school different branch code
+    private String branch;
+    
+    @Column(name = "CLASS_NAME") //section  batchNO  class room
+    private String className;
+    
+    @Column(name = "HOSTEL") //day
+    private String dayHostel;	       
 }
+
